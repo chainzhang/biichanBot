b49bbfe4d76e212b555576e6ba357cf83b6d212b
chainzhang <zokei.chotei@gmail.com>
2014-11-19 11:23:07 +0900

    update brain setting

--- bin/slack.sh
+++ bin/slack.sh
@@ -16,6 +16,8 @@ function slack_start {
     export HUBOT_SLACK_CHANNELMODE="whitelist"
     export HUBOT_SLACK_CHANNELS="random,general,b3m"
     export HUBOT_SLACK_LINK_NAMES="1"
+    export REDIS_URL="redis://localhost:6379/bii:slack:storage"
+    # redis://<host>:<port>[/<brain_prefix>]
 
     PORT=$slack_port bin/hubot --adapter slack > logs/hubot.slack.log 2>&1 &
     if [ $? -eq 0 ]; then
